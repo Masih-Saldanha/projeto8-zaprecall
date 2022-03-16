0e00a77037078aa0e4d0a8e91761eb0011175372
Masih-Saldanha <masih.saldanha@gmail.com>
2022-03-16 01:15:30 -0300

    Base do projeto pronta (CSS Reset, Styles, Head, App e etc. com configuraçoes iniciais)

---
+++ src/componentes/App.js
@@ -0,0 +1,8 @@
+export default function App() {
+    return (
+        <>
+        <h1>Teste h1</h1>
+        <h2>Teste h2</h2>
+        </>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ReactDom from "react-dom";
+import App from "./componentes/App";
+
+const root = document.querySelector(".root");
+ReactDom.render(<App />, root);
