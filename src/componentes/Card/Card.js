513d5b73a5acbbcf43bb1a95a49c99a8a428f243
Masih-Saldanha <masih.saldanha@gmail.com>
2022-03-19 17:10:33 -0300

    Ajustes finos

--- src/componentes/Card/Card.js
+++ src/componentes/Card/Card.js
@@ -2,19 +2,29 @@ import "../Card/Card.css"
 import Setinha from "../../assets/setinha.png"
 
 export default function Card(props) {
-    const {numeroCard, question} = props;
-    // return (
-    //     <section className="card">
-    //         <article className="frontCard">
-    //             <p className="frontCardTitle">Pergunta {numeroCard}</p>
-    //             <ion-icon name="play-outline"></ion-icon>
-    //         </article>
-    //     </section>
-    // )
+    const { numeroCard, question, answer } = props;
     return (
-        <section className="cardQuestion">
-                <p className="question">{question}</p>
-                <img className="setinha" src={Setinha} alt="setinha" />
+        <section className="card">
+            <article className="frontCard">
+                <p className="frontCardTitle">Pergunta {numeroCard}</p>
+                <ion-icon name="play-outline"></ion-icon>
+            </article>
         </section>
     )
+    // return (
+    //     <section className="cardQuestion">
+    //             <p className="question">{question}</p>
+    //             <img className="setinha" src={Setinha} alt="setinha" />
+    //     </section>
+    // )
+    // return (
+    //     <section className="cardQuestion">
+    //         <p className="question">{answer}</p>
+    //         <menu className="buttonsMenu">
+    //             <button className="redButton" >Não lembrei</button>
+    //             <button className="yellowButton" >Quase não lembrei</button>
+    //             <button className="greenButton" >Zap!</button>
+    //         </menu>
+    //     </section>
+    // )
 }
